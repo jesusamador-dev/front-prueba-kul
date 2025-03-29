d3424ce6982bfebea63738602e3d3cad56496768
Jesus Macias <jesus.macias.amador@gmail.com>
2025-03-28 23:02:37 -0600

    fix: se evitan las dobles solicitudes

--- src/app/transactions/presentation/hooks/useFetechTransactions.ts
+++ src/app/transactions/presentation/hooks/useFetechTransactions.ts
@@ -6,14 +6,18 @@ import { FetchTransactionsGateway } from '@/app/transactions/infrastructure/gate
 import { RSADecryptor } from '@/shared/infrastructure/encryption/RSADecryptor'
 import { useTransactionStore } from '@/app/transactions/presentation/state/useTransactionStore'
 import { useKeyPairStore } from '@/shared/presentation/state/useKeyPairState'
+import { useRef } from 'react'
 
 export const useFetchTransactions = () => {
   const setTransactions = useTransactionStore(state => state.setTransactions)
   const keyPair = useKeyPairStore(state => state.keyPair)
+  const hasFetched = useRef(false)
 
   useEffect(() => {
-    if (!keyPair) return;
+    if (!keyPair || hasFetched.current) return;
+
     const fetchTransactions = async () => {
+      hasFetched.current = true;
       const gateway = new FetchTransactionsGateway()
       const decryptor = new RSADecryptor(keyPair.private_key)
       const useCase = new GetAllTransactionsUseCase(gateway, decryptor)
@@ -29,3 +33,4 @@ export const useFetchTransactions = () => {
     fetchTransactions()
   }, [setTransactions, keyPair])
 }
+
